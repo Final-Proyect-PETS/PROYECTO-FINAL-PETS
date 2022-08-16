c027e188295249eec17efc81ab716144aa9bc6b8
JuanjoCieri <juin@hotmail.com.ar>
2022-08-16 20:46:58 -0300

    Falta terminar Validacion Form RegisterUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import {Routes, Route, BrowserRouter } from "react-router-dom"
 import './App.css';
 import LandingPage from "./components/LandingPage"
+import Register from "./components/Register";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
         <Route path={'/'} element={<LandingPage />}/>
+        <Route path={'/register'} element={<Register />}/>
       </Routes>
     </BrowserRouter>  
   );
