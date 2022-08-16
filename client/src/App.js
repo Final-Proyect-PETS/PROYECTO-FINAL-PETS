4990bb8859f5d30e35805394ae62c2cd4519c723
tablonimus <lautamartin1995@gmail.com>
2022-08-16 20:49:06 -0300

    git ignore agregado, nav bar y home en proceso

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {Routes, Route, BrowserRouter } from "react-router-dom"
 import './App.css';
+import Home from "./components/Home";
 import LandingPage from "./components/LandingPage"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
         <Routes>
         <Route path={'/'} element={<LandingPage />}/>
+        <Route path={'/home'} element={<Home />}/>
       </Routes>
     </BrowserRouter>  
   );
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './NavBar/NavBar'
+
+export default function Home() {
+  return (
+    <>
+    <NavBar/>
+    <div>Home cOMPONENT</div>
+    </>
+  )
+}
