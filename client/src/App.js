683685be0eaa3bc33a20f31567a9f9f1a7ba6120
tablonimus <lautamartin1995@gmail.com>
2022-08-17 16:18:26 -0300

    ruta detail y fix nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import {Routes, Route, BrowserRouter } from "react-router-dom"
-import './App.css';
-import Home from "./components/Home";
-import LandingPage from "./components/LandingPage"
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import LandingPage from "./components/LandingPage";
+import UserDetail from "./components/UserDetail/UserDetail";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-        <Route path={'/'} element={<LandingPage />}/>
-        <Route path={'/home'} element={<Home />}/>
+      <Routes>
+        <Route path={"/"} element={<LandingPage />} />
+        <Route path={"/home"} element={<Home />} />
+        <Route path={"/userDetail"} element={<UserDetail />} />
+        <Route path={"/about"} element={<About />} />
       </Routes>
-    </BrowserRouter>  
+    </BrowserRouter>
   );
 }
 
