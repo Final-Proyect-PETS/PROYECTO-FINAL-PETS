57da2f289c5a0859d18d089e73b7142e703910e8
JuanjoCieri <juin@hotmail.com.ar>
2022-08-18 11:35:34 -0300

    Estilos navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -37,17 +37,17 @@ export default function NavBar() {
     <section className="flex justify-center h-14">
       <div className="bg-orange-300 flex justify-between border-b-4 border-white w-screen justify-center content-around items-center px-4">
         <div className="flex">
-          <h1 className="text-white text-2xl font-sans">Happy Tails</h1>
+          <h1 className="text-white text-2xl font-semibold">Happy Tails</h1>
         </div>
         <div className="flex w-80 justify-around">
           <Link to={"/home"}>
-            <h2 className="text-black text-2xl font-sans">Home</h2>
+            <h2 className="text-black text-2xl font-semibold">Home</h2>
           </Link>
           <Link to={"/algo"}>
-            <h2 className="text-black text-2xl font-sans">Algo</h2>
+            <h2 className="text-black text-2xl font-semibold">Algo</h2>
           </Link>
           <Link to={"/about"}>
-            <h2 className="text-black text-2xl font-sans">About</h2>
+            <h2 className="text-black text-2xl font-semibold">About</h2>
           </Link>
         </div>
         <div className="flex">
@@ -57,7 +57,7 @@ export default function NavBar() {
           </form>
         </div>
         <div className="flex gap-3 items-center">
-          <p className="text-white">Lautaro Martín</p>
+          <p className="text-gray-50 font-semibold">Lautaro Martín</p>
           <img src="https://img.freepik.com/fotos-premium/hombre-caucasico-joven-persona-pared-blanca-apuntando-mano-camisa-blanco-orgulloso-confiado_1187-76423.jpg?w=740" alt="logo" width="9px" class="mx-auto object-cover rounded-full h-9 w-9 "/>
         </div>
       </div>
