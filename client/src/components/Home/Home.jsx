c8733faaeb819aabee28b73a6d3e206dfa5390bc
Tomas <91784094+Tomas000000@users.noreply.github.com>
2022-08-18 13:55:21 -0300

    limpieza

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
-import SideBar from '../SideBar/SideBar'
 
 export default function Home() {
   return (
     <>
     <NavBar/>
-    <SideBar/>
     <div></div>
     </>
   )
