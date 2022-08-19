990ce6f2f8e5f1c7413e4f4fb8792cd723d99cbb
Juan Manuel Martinez <99848537+jmanum95@users.noreply.github.com>
2022-08-19 14:22:14 -0300

    modifique las actions de filtros

--- client/src/redux/Actions/actionTypes.js
+++ client/src/redux/Actions/actionTypes.js
@@ -12,13 +12,4 @@ export const GET_PET_BY_NAME = "GET_PET_BY_NAME"; //ok
 export const POST_PET = "POST_PET";
 export const POST_USER = "POST_USER";
 // FILTERS -----------------
-export const SORT_ASC_AGE = "SORT_ASC_AGE"
-export const SORT_ASC_CREATED = "SORT_ASC_CREATED"
-export const SORT_DESC_CREATED = "SORT_DESC_CREATED"
-export const SORT_DESC_AGE = "SORT_DESC_AGE"
-export const FILTER_BY_PLACE = "FILTER_BY_PLACE"
-export const FILTER_BY_TYPE = "FILTER_BY_TYPE"
-export const FILTER_BY_CASTRAED = "FILTER_BY_CASTRAED"
-export const FILTER_BY_VACCINATION = "FILTER_BY_VACCINATION"
-export const FILTER_BY_SIZE = "FILTER_BY_SIZE"
-export const FILTER_BY_AGE = "FILTER_BY_AGE"
+export const FILTER_BY_QUERY = "FILTER_BY_QUERY"
