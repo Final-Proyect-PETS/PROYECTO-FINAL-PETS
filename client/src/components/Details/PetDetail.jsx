e0ede1d1623b4a4bfeda73f6a289202146e11c04
Florencia Re <florencia.re@hotmail.com>
2022-08-21 22:16:16 -0300

    estilos pet details

--- client/src/components/Details/PetDetail.jsx
+++ client/src/components/Details/PetDetail.jsx
@@ -8,31 +8,79 @@ import { getPetDetail } from "../../redux/Actions";
 export default function PetDetail() {
 
 
-    let { id } = useParams()
-    const dispatch = useDispatch()
+  let { id } = useParams()
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(getPetDetail(id));
-    }, [ dispatch, id ]);
+  useEffect(() => {
+    dispatch(getPetDetail(id));
+  }, [dispatch, id]);
 
-    const petDetail = useSelector((state) => state.petDetail);
+  const petDetail = useSelector((state) => state.petDetail);
 
-    return (
-        <div>
-            <NavBar />
-            <h1>Detalles 🐶</h1>
-            <Link  to="/updatepet">
-                  <button>:pencil:</button>
-            </Link>
-            <div>
-                <h2>{petDetail.name}</h2>
-                {/* faltaria agregar el pool de images q viene como array */}
-                <img src={petDetail.image} alt="imagen mascota" />
-                <h3>Descripcion: {petDetail.description}</h3>
-                <h3>Lugar donde se encuntra: {petDetail.place}</h3>
-                <h3>Medida aproximada: {petDetail.size}</h3>
-                <h3>¿Está vacunado?: {petDetail.vaccination}</h3>
-            </div>
+  return (
+    <div>
+      <NavBar />
+      <h1 className="flex justify-center font-semibold text-3xl mt-3">Detalles 🐶</h1>
+
+      <div className="flex w-2/3 my-10 ml-60 flex-row border-2 border-black">
+        <div className="flex flex-col w-1/2 m-3 items-center gap-3">
+          <Link to="/updatepet">
+            <button>Editar</button>
+          </Link>{/* faltaria agregar el pool de images q viene como array */}
+          <img
+            src={petDetail.image}
+            alt="imagen mascota"
+            // width="500px"
+            className="w-96"
+          />
+          <h3 className="text-xs">Descripcion: {petDetail.description}</h3>
         </div>
-    )
+        <div className="flex flex-col w-1/2">
+          <div className="flex flex-col items-center w-full h-1/3 justify-center">
+            <h2 className="font-semibold text-2xl">{petDetail.name}</h2>
+            <h2 className="font-semibold">En</h2>
+            <h3 className="font-semibold">{petDetail.place}</h3>
+          </div>
+          <div className="flex flex-wrap w-full h-1/2 justify-center items-center border-y border-black">
+            <div className="w-1/2 h-1/2 flex justify-center items-center border-b">
+              <h3 className="font-semibold">
+                Tamaño:{" "}
+                {petDetail.size === "big"
+                  ? "Grande"
+                  : petDetail.size === "medium"
+                    ? "Mediano"
+                    : "Chico"}
+              </h3>
+            </div>
+            <div className="w-1/2 h-1/2 flex justify-center items-center border-b">
+              <h3 className="font-semibold">
+                ¿Está vacunado?:{" "}
+                {petDetail.vaccination === "yes"
+                  ? "Sí"
+                  : petDetail.vaccination === "no"
+                    ? "No"
+                    : "No se sabe"}
+              </h3>
+            </div>
+            <h1 className="absolute flex justify-center items-center">Datos</h1>
+            <div className="w-1/2 h-1/2 flex justify-center items-center border-t">
+              <h3 className="font-semibold">Edad: {petDetail.age} años</h3>
+            </div>
+            <div className="w-1/2 h-1/2 flex justify-center items-center border-t">
+              <h3 className="font-semibold">
+                Castrado: {petDetail.castrated === true ? "Si" : "No"}
+              </h3>
+            </div>
+          </div>
+          <div className="flex justify-center items-center h-1/5">
+            <Link to="/adopt/">
+              <button class="bg-yellow-500 hover:bg-green-700 text-white font-bold py-5 px-4 border border-yellow-700 rounded">
+                ADOPTAR
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
