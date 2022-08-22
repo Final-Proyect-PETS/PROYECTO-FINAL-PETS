05bae333c096c6e4aa96258e568c00865b16836b
Florencia Re <florencia.re@hotmail.com>
2022-08-21 23:34:05 -0300

    estilos registro de usuario

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -35,7 +35,7 @@ export default function NavBar() {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-amber-800 w-screen">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/home" className="flex items-center">
-          <img src="https://cdn-icons-png.flaticon.com/512/194/194279.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <img src="https://cdn-icons-png.flaticon.com/512/194/194279.png" className="mr-3 h-6 sm:h-9" alt="logo icon" />
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Happy Tails</span>
         </Link>
         <div className="flex items-center md:order-2">
