011c332df0d1f6f7b91f8c93a360edb59b91aa76
Manuel Müller <manumu99@hotmail.com>
2022-08-23 19:24:17 -0300

    todo andando... en teoria v2.0

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -10,47 +10,11 @@ const router = Router();
 const postPet = require("./posts");
 const patchPet = require("./patch")
 const patchUser = require("./patch");
-// const jwt = require("jsonwebtoken");
 const errorHandler = require("../utils/middlewares/errorHandler")
-// const verifyToken = require('../utils/middlewares/validateToken');
-
 
 router.use("/home", pets, users, userId, petId, filters, postPet, patchPet, patchUser)
 router.use("/register", register)
 router.use("/login", login)
 router.use(errorHandler);
-
-
-// router.patch("/users/:id", verifyToken, async (req, res, next) => {
-//   const {
-//     first_name,
-//     last_name,
-//     username,
-//     email,
-//     password,
-//     image,
-//     telephone,
-//     about,
-//   } = req.body;
-//   try {
-//     const oneUser = await User.findOne({
-//       _id: req.params.id,
-//     });
-//     await oneUser.update({
-//       first_name,
-//       last_name,
-//       username,
-//       email,
-//       password,
-//       image,
-//       telephone,
-//       about,
-//     });
-//     res.status(200).json("Datos Actualizados Exitosamente 👌");
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 
 module.exports = router;
