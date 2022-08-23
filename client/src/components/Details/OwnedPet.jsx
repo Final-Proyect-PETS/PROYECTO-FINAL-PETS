24f84595d2e1ab232adda04b6d4686a240e5e5f4
tablonimus <lautamartin1995@gmail.com>
2022-08-23 20:40:13 -0300

    botones segun usuario logueado

--- client/src/components/Details/OwnedPet.jsx
+++ client/src/components/Details/OwnedPet.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 //import ubicacion from "../../assets/images/ubicacion.png";
 import { Link } from "react-router-dom";
 //import styles from "./userDetailStyle.css";
@@ -16,40 +17,61 @@ export default function OwnedPet({
   gender,
   isAdopted,
 }) {
+  const loggedUser = useSelector((state) => state.userProfile);
+  const userDetail = useSelector((state) => state.userDetail);
   console.log(isAdopted, "Esta Adoptado?");
+
   return (
     <>
-      <div className="flex items-center py-4 px-4 ">
+      <div className="flex items-center py-4 px-5 ">
         <div className=" flex border-2 border-yellow-700 rounded">
           <div className=" border-yellow-900 border-r-2  flex justify-between p-3 border items-center rounded bg-gray-300">
             <div className=" column items-center mb-4 mr-4 ml-4 ">
-              <div className="">
-                <span className="text-2xl font-bold m-4">{namePet}</span>
+              <div className="flex justify-center">
+                <span className="text-2xl font-bold ">{namePet}</span>
               </div>
 
-              {/* < span className="font-medium text-xs mx-3">
-                Ubicacion: {place}
-              </span> */}
-              {isAdopted === true ? (
-                isAdopted === true ? (
-                  <div className="flex items-center">
-                    <Link to="/adopt/">
-                      <button className="bg-yellow-900 mt-4 hover:bg-green-700 text-white font-bold py-5 px-4 border border-yellow-700 rounded">
-                        ADOPTAR
-                      </button>
-                    </Link>
-                  </div>
-                ) : (
-                  <></>
-                )
-              ) : (
-                <div className="flex items-center">
-                  <Link to="formulario de adopcion">
-                    <button className="bg-yellow-900 mt-4 hover:bg-red-700 justify-center text-white font-bold py-2 px-4 border border-yellow-700 rounded">
-                      OFRECER MASCOTA
+              {""}
+              {loggedUser._id === userDetail._id? isAdopted === false ? (
+                <div className="column items-center">
+                  <Link to="/adopt/">
+                    {" "}
+                    {/* link de mierda -------------------------------------------------------------------------*/}
+                    <button className="bg-yellow-900 mr-4 mt-4 hover:bg-yellow-500 text-white font-bold py-2 px-4 border border-yellow-700 rounded">
+                      TRAMITAR PET
+                    </button>
+                  </Link>
+                  <Link to="/quitadopt/">
+                    {" "}
+                    {/* link de mierda*------------------------------------------------------------------- */}
+                    <button className="bg-yellow-900 mt-4 hover:bg-red-700 text-white font-bold py-2 px-4 border border-yellow-700 rounded">
+                      QUITAR DE ADOPCION
                     </button>
                   </Link>
                 </div>
+              ) : (
+                <Link to="/adopt/">
+                  <button className="bg-yellow-900 mt-4 hover:bg-green-900 text-white font-bold py-2 px-4 border border-yellow-700 rounded">
+                    DAR EN ADOPCION
+                  </button>
+                </Link>
+              ):isAdopted === false ? (
+                <div className="column items-center">
+                  <Link to="/adopt/">
+                    {" "}
+                    {/* link de mierda -------------------------------------------------------------------------*/}
+                    <button className="bg-yellow-900 mr-4 mt-4 hover:bg-green-900 text-white font-bold py-2 px-4 border border-yellow-700 rounded">
+                      ADOPTAR
+                    </button>
+                  </Link>
+               
+                </div>
+              ) : (
+                <Link to="/home">
+                  <button className="bg-yellow-900 mt-4 hover:bg-yellow-500 text-white font-bold py-2 px-4 border border-yellow-700 rounded">
+              TENGO DUEÑO
+                  </button>
+                </Link>
               )}
             </div>
           </div>
