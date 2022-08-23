b52c7657bd968a8bdf3058ed0a635365382a9a9b
Tomas <91784094+Tomas000000@users.noreply.github.com>
2022-08-23 13:29:18 -0300

    placeholder con datos anteriores en patch y ubicacion de usuario en home arreglado

--- client/src/components/Home/UserCard.jsx
+++ client/src/components/Home/UserCard.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { useEffect, React } from "react";
 import { getUserDetail, clearState } from "../../redux/Actions/index";
 
-
+/* console.log() */
 export default function UserCard({
   _id,
   first_name,
