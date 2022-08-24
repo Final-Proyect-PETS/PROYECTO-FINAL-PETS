0177a41dec81016e8e0bb68aceee8277c6f401b4
Manuel Müller <manumu99@hotmail.com>
2022-08-24 10:44:59 -0300

    todo andando en teoria v7.0

--- server/src/db.js
+++ server/src/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 //Model
 //const Persona = require("../src/models/Prueba");
 
-const uri = `mongodb+srv://${process.env.usuario}:${process.env.password}@${process.env.dbname}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USUARIO}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}/?retryWrites=true&w=majority`;
 
 module.exports = () => {
   mongoose
