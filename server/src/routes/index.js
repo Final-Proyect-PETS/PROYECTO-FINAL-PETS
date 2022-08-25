c4af1577fa677433dd61919e7c9fc29e2f9143a8
Juan Manuel Martinez <99848537+jmanum95@users.noreply.github.com>
2022-08-25 12:17:52 -0300

    agregamos ruta POST /mail

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -10,11 +10,13 @@ const router = Router();
 const postPet = require("./posts");
 const patchPet = require("./patch")
 const patchUser = require("./patch");
+const adoptionMail = require("./send-email")
 const errorHandler = require("../utils/middlewares/errorHandler")
 
 router.use("/home", pets, users, userId, petId, filters, postPet, patchPet, patchUser)
 router.use("/register", register)
 router.use("/login", login)
+router.use("/mail", adoptionMail)
 router.use(errorHandler);
 
 module.exports = router;
--- server/src/routes/send-email.js
+++ server/src/routes/send-email.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
 const nodemailer = require('nodemailer')
-const Pets = require("../../models/pets");
-const User = require("../../models/users");
 const router = Router();
 const {NMAILER_PASSWORD} = process.env;
 
-router.post('/send-email',async(req,res)=>{
-    const {first_name, email,telephone,message,username,name} = req.body
-
-    let Dataform= User
-
+router.post('/sendemail',async(req,res)=>{
+    const {email, telephone, message, username, name} = req.body
 
     let transporter = nodemailer.createTransport({
         host:'smtp-mail.outlook.com',
@@ -18,22 +13,31 @@ router.post('/send-email',async(req,res)=>{
         auth:{
             user: 'HAppYTAil5@hotmail.com',
             pass: `${NMAILER_PASSWORD}`
-
         },
         tls:{
             rejectUnauthorized: false
         }
     })
 
+    let contentHTML=`
+    <img src = "https://cdn-icons-png.flaticon.com/512/194/194279.png" style="width:100px;"/>
+    <h1>Info del usuario</h1> 
+    <ul>
+        <li>Nombre:${name}</li>
+        <li>Nombre de usuario:${username}</li>
+        <li>Email:${email}</li>
+        <li>Telefono:${telephone}</li>
+    </ul>
+    <p>${message}</p>`
+
     let info = await transporter.sendMail({
         from:"'HappyTails'<HAppYTAil5@hotmail.com>",
         to: email,
-        subject:'formulario de contacto adopcion',
-        text:'esto es otra prueba'
-    })
+        subject:'Formulario de contacto adopcion',
+        html: contentHTML
+        })
 
     console.log('message sent', info.messageId)
     res.send('se envio correctamente')
 })
-
 module.exports = router;
