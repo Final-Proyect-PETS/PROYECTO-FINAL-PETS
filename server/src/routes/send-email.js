bbc27e7b3a83909e94f61dd982948c10134f8812
Juan Manuel Martinez <99848537+jmanum95@users.noreply.github.com>
2022-08-25 13:26:20 -0300

    acomode req.body

--- server/src/routes/send-email.js
+++ server/src/routes/send-email.js
@@ -4,7 +4,7 @@ const router = Router();
 const {NMAILER_PASSWORD} = process.env;
 
 router.post('/sendemail',async(req,res)=>{
-    const {email, telephone, message, username, name} = req.body
+    const {owner_email, adopter_email, adopter_telephone, message, adopter_username, adopter_name} = req.body
 
     let transporter = nodemailer.createTransport({
         host:'smtp-mail.outlook.com',
@@ -23,16 +23,16 @@ router.post('/sendemail',async(req,res)=>{
     <img src = "https://cdn-icons-png.flaticon.com/512/194/194279.png" style="width:100px;"/>
     <h1>Info del usuario</h1> 
     <ul>
-        <li>Nombre:${name}</li>
-        <li>Nombre de usuario:${username}</li>
-        <li>Email:${email}</li>
-        <li>Telefono:${telephone}</li>
+        <li>Nombre:${adopter_name}</li>
+        <li>Nombre de usuario:${adopter_username}</li>
+        <li>Email:${adopter_email}</li>
+        <li>Telefono:${adopter_telephone}</li>
     </ul>
     <p>${message}</p>`
 
     let info = await transporter.sendMail({
         from:"'HappyTails'<HAppYTAil5@hotmail.com>",
-        to: email,
+        to: owner_email,
         subject:'Formulario de contacto adopcion',
         html: contentHTML
         })
