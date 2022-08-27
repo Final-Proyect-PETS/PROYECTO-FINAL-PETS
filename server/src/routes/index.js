841af84bf8d2df6db379f13048145c8ddfe01297
unknown <cgferreiro1985@gmail.com>
2022-08-27 12:56:08 -0300

    va queriendo

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,13 +6,10 @@ const petId = require("./gets");
 const filters = require("./filters");
 const register = require("./register");
 const login = require("./login");
-const loginGoogle = require("./googlelogin");
 const router = Router();
 const postPet = require("./posts");
 const patchPet = require("./patch");
 const patchUser = require("./patch");
-const adoptionMail = require("./send-email");
-const postImage = require("./posts");
 const payment = require("./payment");
 const errorHandler = require("../utils/middlewares/errorHandler");
 
@@ -25,15 +22,20 @@ router.use(
   filters,
   postPet,
   patchPet,
-  patchUser,
-  postImage
+  patchUser
 );
 
 router.use("/linkpayment", payment);
 router.use("/register", register);
 router.use("/login", login);
-router.use("/", loginGoogle);
-router.use("/mail", adoptionMail);
 router.use(errorHandler);
+
+router.get("/feedback/:idDonor/:donationAmount", (req, res) => {
+  const { payment_id, status } = req.query;
+  const { idDonor, donationAmount } = req.params;
+  console.log("aca estaaaaaa: " + donationAmount); //tengo id del usuario, id del pago, cantidad donada.
+  console.log(status); //hacer que if status === approved, agregar los datos de la donacion a una propiedad del usuario
+  return res.redirect("http://localhost:3000/donations");
+});
 
 module.exports = router;
