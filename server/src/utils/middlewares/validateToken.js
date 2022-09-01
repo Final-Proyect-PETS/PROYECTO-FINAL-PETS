4f097def3b44c488480cb7bdf6665956cd74dc40
ManuMuller <manumu99@hotmail.com>
2022-09-01 12:53:18 -0300

    solucion de errores

--- server/src/utils/middlewares/validateToken.js
+++ server/src/utils/middlewares/validateToken.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // middleware to validate token (rutas protegidas)
 const verifyToken = (req, res, next) => {
   const token = req.headers["token"];
-  if (!token) return res.status(401).json({ error: "Acceso denegado" });
+  if (!token) throw new Error("Acceso denegado");
   try {
     const verified = jwt.verify(token, process.env.SECRET_KEY);
     req.user = verified;
