eafc8d7e987f3877d79db9a9dd5abed191d6e300
Florencia Re <florencia.re@hotmail.com>
2022-09-01 00:44:39 -0300

    notificacion terminada

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -15,6 +15,7 @@ export default function NavBar() {
 
   useEffect(() => {
     dispatch(getUserProfile(id));
+
   }, [dispatch, id]);
 
   const loggedUser = useSelector((state) => state.userProfile);//el loggeduser5 estaba arriba del useEFF, lo puse abajio
@@ -25,6 +26,7 @@ export default function NavBar() {
     localStorage.removeItem("token");
     localStorage.removeItem("id");
   }
+
   function closeHandler(e) {
     e.preventDefault();
     let payload = {
@@ -38,12 +40,11 @@ export default function NavBar() {
 
   let algo = loggedUser?.interestedUsers?.map(e => {
     return {
-      user : allUsers.filter(a => a._id === e.interestedUser)[0],
-      pet : allPets.filter(a => a._id === e.petId)[0],
-      viewState : e.viewState
+      user: allUsers.filter(a => a._id === e.interestedUser)[0],
+      pet: allPets.filter(a => a._id === e.petId)[0],
+      viewState: e.viewState
     }
-  }
-)
+  })
 
   return (
 
@@ -73,33 +74,33 @@ export default function NavBar() {
           </Dropdown.Header>
 
           {loggedUser?.interestedUsers?.length ? (
-          algo.map((iUser) => (
-            iUser.viewState === false ? 
-            <Toast key={iUser?.user?._id}>
-              <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-blue-100 text-blue-500 dark:bg-blue-800 dark:text-blue-200">
-                <img src={iUser?.user?.image} className="h-10 w-10 rounded-full" />
-              </div>
-              <div className="ml-3 text-sm font-normal">
-                <h1>{`${iUser.user.first_name} ${iUser.user.last_name} esta interesado en ${iUser.pet.name}`}</h1>
-                <button
-                  value={iUser?.user?._id}
-                  name={iUser?.pet?._id}
-                  className="text-yellow-500"
-                  onClick={(e) => closeHandler(e)}
-                >
-                Marcar como leida
-                </button>
-                <Link to={`/users/${iUser.user._id}`}>
-                  <h1 className="text-yellow-500">Ver Perfil</h1>
-                </Link>
-              </div>
-              <Toast.Toggle/>
-            </Toast>
-             : <></>
-          ))
-        ) : (
-          <>SIN NOTI</>
-        )}
+            algo?.map((iUser) => (
+              iUser.viewState === false ?
+                <Toast key={iUser.user._id}>
+                  <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-blue-100 text-blue-500 dark:bg-blue-800 dark:text-blue-200">
+                    <img src={iUser.user.image} className="h-10 w-10 rounded-full" alt="imagen de usuario" />
+                  </div>
+                  <div className="ml-3 text-sm font-normal">
+                    <h2>{`${iUser.user.first_name} ${iUser.user.last_name} esta interesado en ${iUser.pet.name}`}</h2>
+                    <button
+                      value={iUser.user._id}
+                      name={iUser.pet._id}
+                      className="text-yellow-500 py-1"
+                      onClick={(e) => closeHandler(e)}
+                    >
+                      Marcar como leida
+                    </button>
+                    <Link to={`/users/${iUser.user._id}`}>
+                      <h2 className="text-yellow-500">Ver Perfil</h2>
+                    </Link>
+                  </div>
+                  <Toast.Toggle />
+                </Toast>
+                : <></>
+            ))
+          ) : (
+            <Dropdown.Item>Sin notificaciones nuevas</Dropdown.Item>
+          )}
 
           <Dropdown.Divider />
           <Link to={"/notifications"}>
@@ -110,9 +111,7 @@ export default function NavBar() {
         <Dropdown
           arrowIcon={false}
           inline={true}
-          label={
-            <Avatar alt="User settings" img={loggedUser.image} rounded={true} />
-          }
+          label={<Avatar alt="User settings" img={loggedUser.image} rounded={true} />}
         >
           <Dropdown.Header>
             <span className="block text-sm">
